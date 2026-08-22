072c2c07bbe8582b143d305da3d3f41efe45081a
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2026-08-22 13:52:50 +0200

    update download link

--- pack.pl
+++ pack.pl
@@ -5,5 +5,5 @@ pack_version(2).
 author('kwon-young', 'https://github.com/kwon-young').
 maintainer('kwon-young', 'https://github.com/kwon-young').
 home('https://github.com/kwon-young/sdlpl').
-download('https://github.com/kwon-young/sdlpl/archive/V*.zip').
+download('https://github.com/kwon-young/sdlpl/releases/*.zip').
 keywords([sdl, sdl3, graphics, games, foreign]).
